3bf2946c625dbd871c7c972e196d47e38ef28226
tjkendev <jake-basu@hotmail.co.jp>
2021-09-04 22:20:42 +0900

    update webpack version

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   module: {
     rules: [
-    {test: /\.css$/, loader: "style!css"}
+      {test: /\.css$/, use: ["style-loader", "css-loader"]}
     ]
   },
   plugins: [
